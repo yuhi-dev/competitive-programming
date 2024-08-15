4952c51567afbf1cc260c69f972616fe36de4564
yuhi-dev <61339614+yuhi-dev@users.noreply.github.com>
2024-08-15 14:20:49 +0900

    commit APG4b

---
+++ JOI_seminer/hensyukyori.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int M;
+    cin >> M;
+    string S;
+    cin >> S;
+    int N;
+    cin >> N;
+    string T;
+    cin >> T;
+
+    
+}
