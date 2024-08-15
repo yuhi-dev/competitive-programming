63131bdcf1d01c04db513e9bf90fab0b2d9de126
yuhi-dev <61339614+yuhi-dev@users.noreply.github.com>
2024-08-15 15:25:12 +0900

    commit joi

---
+++ APG4b/plusorminas.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+
+  // 計算結果を保持する変数
+  int answer = 1;
+
+  for (int i = 0; i < S.size(); i++) {
+
+    if (S.at(i) == '+') {
+      answer++;
+    }
+    if (S.at(i) == '-') {
+      answer--;
+    }
+
+  }
+
+  cout << answer << endl;
+}
--- JOI_seminer/hensyukyori.cpp
+++ JOI_seminer/hensyukyori.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-int main(){
-    int M;
-    cin >> M;
-    string S;
-    cin >> S;
-    int N;
-    cin >> N;
-    string T;
-    cin >> T;
+int M, N, dp[1009][1009];
+string S, T;
 
-    
+int main(){
+    cin >> M >> N;
+    cin >> S >> T;
+    //M = S.size();
+    //N = T.size();
+
+    dp[0][0] = 0;
+    for (int i = 0; i <= M; i++){
+        for (int j = 0; j <= N; j++){
+            if (i >= 1 && j >= 1 && S[i-1] == T[j-1]){
+                dp[i][j] = min(dp[i][j], dp[i-1][j-1]);
+            }
+            else if (i >= 1 && j >= 1){
+                dp[i][j] = min({dp[i][j], dp[i-1][j-1] + 1, dp[i-1][j] + 1, dp[i][j-1] + 1});
+            }
+        }
+    }
+    cout << dp[N][M] << endl;
+    return 0;
 }
